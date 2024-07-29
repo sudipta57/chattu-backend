98082c65ce32969524e0be711357ed769ec41b27
sudipta57 <ghoramiswapna32@gmail.com>
2024-07-29 22:16:58 +0530

    adding the frontend url

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
+    "https://chattu-mern.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
